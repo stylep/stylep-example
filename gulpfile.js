60956a1bb884d6fc4edfd75cb90d628db8c29082
Stephen Way <way.stephen@gmail.com>
2015-12-28 09:43:43 -0800

    Added gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var autoprefixer = require('autoprefixer');
+var postcss = require('gulp-postcss');
+var simplevars = require('postcss-simple-vars');
+var cssimport = require('postcss-import');
+var nested = require('postcss-nested');
+var extend = require('postcss-extend');
+var stylelint = require('stylelint');
+var reporter = require('postcss-reporter');
+
+// Check code style of stylesheets
+gulp.task('css:lint', function() {
+  var stylelintConfig = {
+    "extends": "stylelint-config-suitcss",
+    "rules": {
+      "at-rule-empty-line-before": null,
+      "block-opening-brace-newline-after": null,
+      "comment-empty-line-before": null,
+      "number-leading-zero": null,
+      "string-quotes": "single",
+      "selector-list-comma-newline-after": "always-multi-line",
+      "selector-combinator-space-before": null
+    }
+  }
+  return gulp.src('src/**/*.css')
+  .pipe(postcss([
+      stylelint(stylelintConfig),
+      reporter({ clearMessages: true }),
+  ]));
+});
+
+// Compile css into /example
+gulp.task('css', ['css:lint'], function() {
+  var processors = [
+      cssimport,
+      mixins,
+      simplevars,
+      extend,
+      nested,
+      autoprefixer(['last 2 versions'])
+  ];
+  return gulp.src('src/*.css')
+      .pipe(postcss(processors).on('error', gutil.log))
+      .pipe(gulp.dest('styles'));
+});
+
+// Watch css and compile upon file changed
+gulp.task('watch', function() {
+  gulp.watch('src/*.css', ['css']);
+});
+
